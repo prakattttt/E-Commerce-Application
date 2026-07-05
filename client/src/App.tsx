7cddaee3c905318295c418861d7f2991716611d1
prakat <prakat.dhakall64@gmail.com>
2026-07-05 19:46:03 +0545

    fix: move shop page to its right place

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import Profile from "./pages/Profile";
-import Shop from "./components/Shop";
+import Shop from "./pages/Shop";
 
 function App() {
   return (
