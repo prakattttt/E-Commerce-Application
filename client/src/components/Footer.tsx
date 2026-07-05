6594529d3c0bb8e4ceb7dccf26e81c7c51171ba2
prakat <prakat.dhakall64@gmail.com>
2026-07-05 20:02:10 +0545

    style: change footer gradient

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -5,7 +5,7 @@ import { footerSections } from "./footer/footer.data";
 
 const Footer = () => {
   return (
-    <footer className="mt-24 bg-linear-to-r from-secondary-foreground via-primary to-primary/90 text-primary-foreground">
+    <footer className="mt-24 bg-linear-to-r from-indigo-900 via-indigo-700 to-indigo-500 text-primary-foreground">
       <div className="mx-auto max-w-7xl px-6 py-16">
         <div className="grid gap-12 md:grid-cols-[2fr_3fr]">
           <FooterBrand />
