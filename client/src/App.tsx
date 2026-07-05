ab58b63a6eef6b11548bc1c592f80e5942d5427d
prakat <prakat.dhakall64@gmail.com>
2026-07-05 19:41:10 +0545

    feat: add additional routes for different pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
+import Profile from "./pages/Profile";
+import Shop from "./components/Shop";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
         <Routes>
           <Route path="/" element={<MainLayout />}>
             <Route index element={<HomePage />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/shop" element={<Shop />} />
           </Route>
         </Routes>
       </BrowserRouter>
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,7 +1,9 @@
+import React from 'react'
+
 const HomePage = () => {
   return (
-    <div>Hello World!</div>
+    <div>HomePage</div>
   )
 }
 
-export default HomePage;
+export default HomePage
