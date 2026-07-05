94944ab4fef4aac4f9d7a0da2a0e252e250f4b95
prakat <prakat.dhakall64@gmail.com>
2026-07-05 18:28:15 +0545

    feat: add menus for navbar

---
+++ client/src/components/nav/Menus.tsx
@@ -0,0 +1,44 @@
+import { NavLink } from "react-router-dom";
+
+{
+  /*Menu Items*/
+}
+const menus = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "Shop",
+    path: "/shop",
+  },
+  {
+    name: "Profile",
+    path: "/profile",
+  },
+];
+
+const Menus = () => {
+  return (
+    <ul className="hidden items-center gap-8 font-medium md:flex">
+      {/*Mapping of Menu Items*/}
+      {menus.map((menu) => (
+        <li key={menu.name}>
+          {/*NavLink to track active nav state*/}
+          <NavLink
+            to={menu.path}
+            className={({ isActive }) =>    
+              `transition-colors duration-200 ${
+                isActive ? "text-primary" : "text-foreground hover:text-primary"
+              }`
+            }
+          >
+            {menu.name}
+          </NavLink>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Menus;
