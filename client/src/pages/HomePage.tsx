e9859920ddb4b9182ebea1faa1f50142f110737a
prakat <prakat.dhakall64@gmail.com>
2026-07-06 20:42:56 +0545

    feat: add the newly created sections to the homepage

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,9 +1,15 @@
-import HeroSection from "../components/main/HeroSection"
+import HeroSection from "../components/main/HeroSection";
+import Categories from "../components/main/Categories";
+import Trending from "../components/main/Trending";
 
 const HomePage = () => {
   return (
-    <HeroSection />
-  )
-}
+    <>
+      <HeroSection />
+      <Categories />
+      <Trending />
+    </>
+  );
+};
 
-export default HomePage
+export default HomePage;
