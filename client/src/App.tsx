cb23b0fdb9dc77d46f4db32d8d394d94abe63510
prakat <prakat.dhakall64@gmail.com>
2026-07-09 19:40:00 +0545

    feat: include the authentication throughout the routes of our project

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,16 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import Toaster from "./components/ui/Toaster";
+import { useEffect } from "react";
+import useAuth from "./features/auth/hooks/useAuth";
 
 function App() {
+  const { checkAuth } = useAuth();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
   return (
     <BrowserRouter>
       <Toaster />
