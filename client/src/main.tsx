4fd09f095fcc30e4ab06e1b17bd44b53b00d7f64
prakat <prakat.dhakall64@gmail.com>
2026-07-10 21:09:22 +0545

    feat: create a component to scroll the page to the top on route change

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,10 +4,12 @@ import "./index.css";
 import "./App.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./components/ui/ScrollToTop.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </StrictMode>,
