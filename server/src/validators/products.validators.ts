31db7d8e84b21b0a01fd533dee42da7b00ed61e5
prakat <prakat.dhakall64@gmail.com>
2026-07-27 22:54:07 +0545

    fix: change the createProductSchema for fomData inputs and apply transformation for badge and originalPrice

--- server/src/validators/products.validators.ts
+++ server/src/validators/products.validators.ts
@@ -1,21 +1,30 @@
 import { z } from "zod";
 
-export const createProductSchema = z.object({
-  name: z.string().min(3).max(20),
+export const createProductSchema = z
+  .object({
+    name: z.string().trim().min(3).max(20),
 
-  description: z.string().min(10),
+    description: z.string().trim().min(10),
 
-  price: z.number().min(0),
+    price: z.coerce.number().min(0),
 
-  originalPrice: z.number().min(0).optional(),
+    originalPrice: z.coerce.number().min(0).optional(),
 
-  stock: z.number().min(0),
+    stock: z.coerce.number().min(0),
 
-  brand: z.string().min(2),
+    brand: z.string().trim().min(2),
 
-  category: z.string().min(1),
+    category: z.string(),
 
-  featured: z.boolean().default(false),
+    badge: z.string().trim().optional(),
 
-  flashSale: z.boolean().default(false),
-});
+    featured: z.coerce.boolean().default(false),
+
+    flashSale: z.coerce.boolean().default(false),
+  })
+  .transform((data) => ({
+    ...data,
+    badge: data.badge ?? "",
+    originalPrice: data.originalPrice ?? data.price,
+  }));
+
