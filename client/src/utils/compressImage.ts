211a36d282840335cd10b0d247a49b9fa2034bcf
prakat <prakat.dhakall64@gmail.com>
2026-08-04 23:48:27 +0545

    feat: add a utility to compress the image to 5MB to match the backend without any error

---
+++ client/src/utils/compressImage.ts
@@ -0,0 +1,15 @@
+import imageCompression from "browser-image-compression";
+
+export const compressImage = async (file: File) => {
+  const options = {
+    maxSizeMB: 5,
+    maxWidthOrHeight: 1920,
+    useWebWorker: true,
+  };
+
+  return await imageCompression(file, options);
+};
+
+export const compressImages = async (files: File[]) => {
+  return Promise.all(files.map((file) => compressImage(file)));
+};
