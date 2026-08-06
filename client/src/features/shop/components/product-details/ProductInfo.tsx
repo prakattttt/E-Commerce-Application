132f3e5e78fde06fb7715c3be4d498b9264a684c
prakat <prakat.dhakall64@gmail.com>
2026-08-06 21:55:48 +0545

    feat: add the add to cart feature in individual product section

--- client/src/features/shop/components/product-details/ProductInfo.tsx
+++ client/src/features/shop/components/product-details/ProductInfo.tsx
@@ -14,6 +14,8 @@ import RatingStars from "./RatingStars";
 import Price from "./Price";
 import StockStatus from "./StockStatus";
 import QuantitySelector from "./QuantitySelector";
+import { toast } from "sonner";
+import useCart from "../../../cart/hooks/useCart";
 
 interface ProductInfoProps {
   product: IProduct;
@@ -21,47 +23,51 @@ interface ProductInfoProps {
 
 const ProductInfo = ({ product }: ProductInfoProps) => {
   const [quantity, setQuantity] = useState(1);
+  const { addItem } = useCart();
+
+  const addCart = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+
+    await addItem(product._id, quantity);
+
+    toast.success(`${product.name} added to cart`);
+  };
 
   return (
     <div className="space-y-7">
       <ProductBadge badge={product.badge} featured={product.featured} />
-
       {/* Name */}
       <h1 className="font-display text-4xl font-bold leading-tight">
         {product.name}
       </h1>
-
       {/* Brand + Category */}
       <div className="flex flex-wrap items-center gap-3 text-sm text-muted-foreground">
         <span>{product.brand}</span>
         <span>•</span>
         <span>{product.category.name}</span>
       </div>
-
       <RatingStars rating={product.rating} reviews={product.reviews} />
-
       <Price
         price={product.price}
         originalPrice={product.originalPrice ?? product.price}
       />
-
       <StockStatus stock={product.stock} />
-
       {/* Description */}
       <div>
         <h3 className="mt-7 mb-2 font-semibold">Description</h3>
         <p className="leading-7 text-muted-foreground">{product.description}</p>
       </div>
-
       <QuantitySelector
         quantity={quantity}
         onChange={setQuantity}
         max={product.stock > 0 ? product.stock : 1}
       />
-
       {/* Buttons */}
       <div className="flex flex-wrap gap-3">
-        <button className="flex flex-1 items-center justify-center gap-2 rounded-xl bg-primary px-3 lg:px-6 py-4 font-semibold text-primary-foreground transition hover:opacity-90">
+        <button
+          className="flex flex-1 items-center justify-center gap-2 rounded-xl bg-primary px-3 lg:px-6 py-4 font-semibold text-primary-foreground transition hover:opacity-90"
+          onClick={addCart}
+        >
           <ShoppingCart size={20} />
           Add to Cart
         </button>
@@ -74,7 +80,6 @@ const ProductInfo = ({ product }: ProductInfoProps) => {
           <Share2 size={20} />
         </button>
       </div>
-
       Benefits
       <div className="space-y-4 rounded-2xl border border-border bg-card p-5">
         <div className="flex items-center gap-3">
