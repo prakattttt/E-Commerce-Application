4d17ac8a08ad75f8aff9dcaddd8f9b365d03ce61
prakat <prakat.dhakall64@gmail.com>
2026-08-12 14:08:47 +0545

    fix: add and edit the zod schema in both frontend and backend

--- client/src/features/contact/types/schemas/contact.schemas.ts
+++ client/src/features/contact/types/schemas/contact.schemas.ts
@@ -1,10 +1,23 @@
-import z from "zod";
+import { z } from "zod";
 
 export const contactSchema = z.object({
-  name: z.string().trim().min(1, "Name is required"),
-  email: z.email("Please enter a valid email"),
-  subject: z.string().min(1, "Please select a topic"),
-  message: z.string().trim().min(10, "Message must be at least 10 characters"),
+  name: z
+    .string()
+    .trim()
+    .min(1, "Name is required")
+    .max(100, "Name is too long"),
+
+  email: z.email(),
+
+  subject: z.enum(["order", "product", "payment", "account", "other"], {
+    message: "Please select a valid subject",
+  }),
+
+  message: z
+    .string()
+    .trim()
+    .min(1, "Message is required")
+    .max(5000, "Message is too long"),
 });
 
-export type ContactFormValues = z.infer<typeof contactSchema>;
+export type ContactData = z.infer<typeof contactSchema>;
--- server/src/validators/contact.validators.ts
+++ server/src/validators/contact.validators.ts
@@ -1,11 +1,23 @@
-import z from "zod";
+import { z } from "zod";
 
 export const contactSchema = z.object({
-  name: z.string().trim().min(1, "Name is required"),
+  name: z
+    .string()
+    .trim()
+    .min(1, "Name is required")
+    .max(100, "Name is too long"),
 
-  email: z.string().trim().email("Invalid email address"),
+  email: z.email(),
 
-  subject: z.enum(["order", "product", "payment", "account", "other"]),
+  subject: z.enum(["order", "product", "payment", "account", "other"], {
+    message: "Please select a valid subject",
+  }),
 
-  message: z.string().trim().min(1, "Message is required"),
+  message: z
+    .string()
+    .trim()
+    .min(1, "Message is required")
+    .max(5000, "Message is too long"),
 });
+
+export type ContactData = z.infer<typeof contactSchema>;
